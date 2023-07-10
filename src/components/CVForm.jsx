2f64c09d7f767ef888b7ba3608eebad8d347a69d
Chyisiang <chyisiang8@gmail.com>
2023-07-10 09:56:19 +0800

    added image

--- src/components/CVForm.jsx
+++ src/components/CVForm.jsx
@@ -4,102 +4,153 @@ import Education from './Education';
 import Experience from './Experience';
 
 class CVForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      phoneNumber: '',
-      schoolName: '',
-      studyTitle: '',
-      studyDate: '',
-      companyName: '',
-      positionTitle: '',
-      tasks: '',
-      workFrom: '',
-      workUntil: '',
-      isEditing: true,
+    constructor(props) {
+        super(props);
+        this.state = {
+            personalImage: null,
+            name: '',
+            email: '',
+            phoneNumber: '',
+            schoolName: '',
+            studyTitle: '',
+            studyDate: '',
+            companyName: '',
+            positionTitle: '',
+            tasks: '',
+            workFrom: '',
+            workUntil: '',
+            isEditing: true,
+        };
+    }
+
+    handleImageChange = (e) => {
+        const file = e.target.files[0];
+        this.setState({ personalImage: file });
     };
-  }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({ isEditing: false });
-  };
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({ isEditing: false });
+    };
 
-  handleEdit = () => {
-    this.setState({ isEditing: true });
-  };
+    handleEdit = () => {
+        this.setState({ isEditing: true });
+    };
 
-  render() {
-    const {
-      name,
-      email,
-      phoneNumber,
-      schoolName,
-      studyTitle,
-      studyDate,
-      companyName,
-      positionTitle,
-      tasks,
-      workFrom,
-      workUntil,
-      isEditing,
-    } = this.state;
+    handleDownload = () => {
+        const {
+            name,
+            email,
+            phoneNumber,
+            schoolName,
+            studyTitle,
+            studyDate,
+            companyName,
+            positionTitle,
+            tasks,
+            workFrom,
+            workUntil,
+        } = this.state;
 
-    return (
-      <div className="CVForm">
-        <h1>CV Form</h1>
+        const cvContent = `Name: ${name}
+            Email: ${email}
+            Phone Number: ${phoneNumber}
+            School Name: ${schoolName}
+            Study Title: ${studyTitle}
+            Study Date: ${studyDate}
+            Company Name: ${companyName}
+            Position Title: ${positionTitle}
+            Tasks: ${tasks}
+            Work From: ${workFrom}
+            Work Until: ${workUntil}`;
 
-        {isEditing ? (
-          <form onSubmit={this.handleSubmit}>
-            <Personal
-              name={name}
-              email={email}
-              phoneNumber={phoneNumber}
-              handleChange={this.handleChange}
-            />
-            <Education
-              schoolName={schoolName}
-              studyTitle={studyTitle}
-              studyDate={studyDate}
-              handleChange={this.handleChange}
-            />
-            <Experience
-              companyName={companyName}
-              positionTitle={positionTitle}
-              tasks={tasks}
-              workFrom={workFrom}
-              workUntil={workUntil}
-              handleChange={this.handleChange}
-            />
+        const blob = new Blob([cvContent], { type: 'text/plain' });
 
-            <button type="submit">Generate CV Preview</button>
-          </form>
-        ) : (
-          <div>
-            <h2>CV Preview</h2>
-            <p>Name: {name}</p>
-            <p>Email: {email}</p>
-            <p>Phone Number: {phoneNumber}</p>
-            <p>School Name: {schoolName}</p>
-            <p>Study Title: {studyTitle}</p>
-            <p>Study Date: {studyDate}</p>
-            <p>Company Name: {companyName}</p>
-            <p>Position Title: {positionTitle}</p>
-            <p>Tasks: {tasks}</p>
-            <p>Work From: {workFrom}</p>
-            <p>Work Until: {workUntil}</p>
-            <button onClick={this.handleEdit}>Edit</button>
-          </div>
-        )}
-      </div>
-    );
-  }
+        const url = URL.createObjectURL(blob);
+
+        const downloadLink = document.createElement('a');
+        downloadLink.href = url;
+        downloadLink.download = 'cv.txt';
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+
+        document.body.removeChild(downloadLink);
+        URL.revokeObjectURL(url);
+    };
+
+    render() {
+        const {
+            name,
+            email,
+            phoneNumber,
+            schoolName,
+            studyTitle,
+            studyDate,
+            companyName,
+            positionTitle,
+            tasks,
+            workFrom,
+            workUntil,
+            personalImage,
+            isEditing,
+        } = this.state;
+
+        return (
+            <div className="CVForm">
+                <h1>CV Form</h1>
+
+                {isEditing ? (
+                    <form onSubmit={this.handleSubmit}>
+                        <Personal
+                            name={name}
+                            email={email}
+                            phoneNumber={phoneNumber}
+                            personalImage={personalImage}
+                            handleChange={this.handleChange}
+                            handleImageChange={this.handleImageChange}
+                        />
+                        <Education
+                            schoolName={schoolName}
+                            studyTitle={studyTitle}
+                            studyDate={studyDate}
+                            handleChange={this.handleChange}
+                        />
+                        <Experience
+                            companyName={companyName}
+                            positionTitle={positionTitle}
+                            tasks={tasks}
+                            workFrom={workFrom}
+                            workUntil={workUntil}
+                            handleChange={this.handleChange}
+                        />
+
+                        <button type="submit">Generate CV Preview</button>
+                    </form>
+                ) : (
+                    <div>
+                        <h2>CV Preview</h2>
+                        <p>Name: {name}</p>
+                        <p>Email: {email}</p>
+                        <p>Phone Number: {phoneNumber}</p>
+                        <p>School Name: {schoolName}</p>
+                        <p>Study Title: {studyTitle}</p>
+                        <p>Study Date: {studyDate}</p>
+                        <p>Company Name: {companyName}</p>
+                        <p>Position Title: {positionTitle}</p>
+                        <p>Tasks: {tasks}</p>
+                        <p>Work From: {workFrom}</p>
+                        <p>Work Until: {workUntil}</p>
+                        <button onClick={this.handleEdit}>Edit</button>
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default CVForm;
--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Personal = ({ name, email, phoneNumber, handleChange }) => {
+const Personal = ({ name, email, phoneNumber, personalImage, handleChange, handleImageChange }) => {
   return (
-    <div>
+    <div className='personal'>
       <h2>General Information</h2>
       <label>
         Name:
@@ -33,6 +33,17 @@ const Personal = ({ name, email, phoneNumber, handleChange }) => {
           onChange={handleChange}
         />
       </label>
+      <br />
+      <label>
+        Personal Image:
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+      </label>
+      {personalImage && (
+        <div className='image-preview'>
+          <h4>Selected Image:</h4>
+          <img src={URL.createObjectURL(personalImage)} alt="Personal" />
+        </div>
+      )}
     </div>
   );
 };
