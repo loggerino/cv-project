541d5bf7f255e64edddb9e780d6892e9144dfef8
Chyisiang <chyisiang8@gmail.com>
2023-07-14 07:47:01 +0800

    replaced class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
 import './App.css'
 import CVForm from './components/CVForm';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CVForm />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <CVForm />
+    </div>
+  );
+};
 
 
 export default App
